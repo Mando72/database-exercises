ccb2fbf335367933164436f738f77bd8256d5ff1
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-08-13 09:23:41 -0500

    compmpleted the albums/table db exercise

---
+++ albums_migration.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(200) NOT NULL,
+  name VARCHAR(200)NOT NULL,
+  release_date INT UNSIGNED NOT NULL,
+  sales DECIMAL(6, 2),
+  genre VARCHAR(200),
+  PRIMARY KEY (id),
+  UNIQUE(name,artist)
+);
+
+DESCRIBE albums;
